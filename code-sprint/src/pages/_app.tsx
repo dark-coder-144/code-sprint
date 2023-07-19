ac0dbae18af6dfe7ac82d81d2cf47e92fd8e435b
K SHRAVAN <86780079+dark-coder-144@users.noreply.github.com>
2023-07-19 19:55:29 +0530

    First Commit

---
+++ code-sprint/src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import {RecoilRoot} from "recoil" 
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactTostify.css"
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Head>
+        <title>Code-Sprint</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'/>
+        <link rel='icon' href='/favicon.png'/>
+        <meta name='description' content='A coding website that has good coding questions and solutions to it'/>
+      </Head>
+      <ToastContainer/>
+      <Component {...pageProps}/>
+    </RecoilRoot>
+  )
+}
